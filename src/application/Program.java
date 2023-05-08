c9eed9628a91c1bbc23c33490cf39b2d50803fd8
jpclrocha <jpclrocha@gmail.com>
2023-05-08 16:17:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(1,"joao","email", LocalDate.now(),2000.0,obj);
+        System.out.println(seller);
     }
 }
